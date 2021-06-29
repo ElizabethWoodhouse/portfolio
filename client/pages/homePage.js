a44f2a8e6a3d9c3c5b98bb659b213e1823acb077
Elizabeth Woodhouse <elizabethjpwoodhouse@gmail.com>
2021-06-28 20:42:46 -0400

    homePage: placeholder content

--- client/pages/homePage.js
+++ client/pages/homePage.js
@@ -1,3 +1,16 @@
-const homePage = `<p>homepage</p>`;
+const homePage = `<div class='home-page-content'>
+<div class='about-me-box'>
+<h3>About Me</h3>
+<p>Lorem ipsum dolor sit amet, per no ocurreret adipiscing referrentur, summo zril nec eu, pri legere nonumes at.
+Et mea dicta nostro euripidis. Usu et modus possit diceret. His iisque oblique alterum no.
+Rebum consequuntur has id, mutat vivendo in cum.<br>Vis doctus consectetuer ne, te nec facete docendi.
+Vix graeco tamquam utroque ut. Nullam suavitate ut vix, ea est meis velit altera, sea delicata mediocrem ei.
+Per facer scaevola forensibus at, eu qui tota dicat, enim graece vidisse mea te. Luptatum omittantur vis ei.
+</p>
+</div>
+<div class='about-me-box'>
+<p>Image Placeholder</p>
+</div>
+</div>`;
 
 export default homePage;
